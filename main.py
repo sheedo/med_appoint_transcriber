fc9c3ec2ddaa23f5dabe8c49c077d4d088f6c8e9
Erik Pilkington <erik.pilkington@gmail.com>
2017-03-25 11:30:35 -0400

    I did something!

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, send_from_directory, url_for
 import tempfile
 
 app = Flask(__name__)
 
 @app.route("/")
 def run_it():
-    return render_template("index.html")
+    return render_template("index.html", button=url_for('static', filename='button.jpg'), url=request.url_root)
 
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     f = request.files['text_file']
-#     temp_file = tempfile.mkstemp()
-#     f.save(temp_file[1])
-#     # Colin ... do something!!!
+@app.route('/upload_it', methods=['GET'])
+def upload_file():
+    result = request.args.get("data");
+
+    print "got result: " + result;
+
+    return ""
 
 if __name__ == "__main__":
     app.run()
