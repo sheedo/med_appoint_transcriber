8ecdd39cfab0de870bd5c93b722cd4bcdbbe20ec
Erik Pilkington <erik.pilkington@gmail.com>
2017-03-25 14:55:02 -0400

    hook up pattern_recog_python stuff into main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template, request, send_from_directory, url_for
+from flask import Flask, render_template, request
 import tempfile
+import RAKE
+import re
 
 app = Flask(__name__)
+
+def clean_sentence(text):
+    path = "words.txt"
+    r = RAKE.Rake(path)
+    k = []
+    for word in text.split("."):
+        k.append(r.run(word))
+
+    clean_string = []
+    for word in k:
+        clean_string.append(re.sub('[^A-Za-z ]+', '', str(word)))
+
+    result_string = ""
+    for word in clean_string:
+        result_string += word + " "
+    return result_string
 
 @app.route("/")
 def run_it():
@@ -14,8 +32,15 @@ def upload_it():
     result = request.args.get("data")
     email_address = request.args.get("email")
 
+    if (result == None or email_address == None):
+        return
+
     print "got result: " + result
     print "with email: " + email_address
+
+    result = clean_sentence(result)
+
+    print "result = " + result;
 
     return ""
 
