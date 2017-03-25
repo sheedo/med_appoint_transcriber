399b8d68c02dc2a3a3f72dce2b44ef10291e5d6e
Erik Pilkington <erik.pilkington@gmail.com>
2017-03-25 14:01:09 -0400

    emails!

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def run_it():
     return render_template("index.html", url=request.url_root)
 
 @app.route('/upload_it', methods=['GET'])
-def upload_file():
-    result = request.args.get("data");
+def upload_it():
+    result = request.args.get("data")
+    email_address = request.args.get("email")
 
-    print "got result: " + result;
+    print "got result: " + result
+    print "with email: " + email_address
 
     return ""
 
